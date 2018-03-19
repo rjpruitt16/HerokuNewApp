5c391f00a78295425cb3bff3e1eb096633e2fc84
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-03-18 21:26:54 -0400

    updated all jobs to the same timezone and made migrations dirs for db error

--- clock.py
+++ clock.py
@@ -5,34 +5,34 @@ from pytz import timezone
 sched = BlockingScheduler()
 central = timezone('US/Central')
 
-@sched.scheduled_job('cron', hour=7)
+@sched.scheduled_job('cron', hour=7, timezone=central)
 def ScrapeNewsWashingtonPost():
     call(["python3", "manage.py", "NewsScraper", "--url",
       "https://www.washingtonpost.com/",
       "--news", "WashingtonPost"])
 
-@sched.scheduled_job('cron', hour=7)
+@sched.scheduled_job('cron', hour=7, timezone=central)
 def ScrapeNewsCNN():
     call(["python3", "manage.py", "NewsScraper", "--url", "https://www.cnn.com/",
       "--news", "CNN"])
 
-@sched.scheduled_job('cron', hour=7)
+@sched.scheduled_job('cron', hour=7, timezone=central)
 def ScrapeNewsHuffingtonPost():
     call(["python3", "manage.py", "NewsScraper", "--url",
      "https://www.huffingtonpost.com/",
       "--news", "HuffingtonPost"])
 
-@sched.scheduled_job('cron', hour=7)
+@sched.scheduled_job('cron', hour=7, timezone=central)
 def ScrapeNewsHuffingtonPost():
     call(["python3", "manage.py", "NewsScraper", "--url",
       "https://www.npr.org/sections/politics/", "--news", "NPR"])
 
-@sched.scheduled_job('cron', hour=7)
+@sched.scheduled_job('cron', hour=7, timezone=central)
 def ScrapeNewsFox():
     call(["python3", "manage.py", "NewsScraper", "--url",
       "http://www.foxnews.com/", "--news", "FoxNews"])
 
-@sched.scheduled_job('cron', hour=7)
+@sched.scheduled_job('cron', hour=7, timezone=central)
 def ScrapeNewsBloomberg():
     call(["python3", "manage.py", "NewsScraper", "--url",
       "https://www.bloomberg.com/", "--news", "Bloomberg"])
